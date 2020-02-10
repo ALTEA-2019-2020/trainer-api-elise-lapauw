47555e911ac122680c2ed94f5657e00e3aed84b6
elise-lapauw <lapauwelise@gmail.com>
2020-02-10 06:54:00 +0100

    Last part

---
+++ src/main/java/com/miage/altea/trainer_api/controller/TrainerController.java
@@ -0,0 +1,41 @@
+package com.miage.altea.trainer_api.controller;
+
+import com.miage.altea.trainer_api.bo.Trainer;
+import com.miage.altea.trainer_api.service.TrainerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/trainers")
+public class TrainerController {
+    @Autowired
+    private final TrainerService trainerService;
+
+    TrainerController(TrainerService trainerService){
+        this.trainerService = trainerService;
+    }
+
+    @GetMapping("/")
+    Iterable<Trainer> getAllTrainers(){
+        return this.trainerService.getAllTrainers();
+    }
+
+    @GetMapping("/{name}")
+    Trainer getTrainer(@PathVariable String name){
+        return  this.trainerService.getTrainer(name);
+    }
+
+    @PutMapping()
+    Trainer updateTrainer(@RequestBody Trainer trainer) {
+        return this.trainerService.createTrainer(trainer);
+    }
+    @PostMapping()
+    Trainer newTrainer(@RequestBody Trainer trainer) {
+        return this.trainerService.createTrainer(trainer);
+    }
+    @DeleteMapping(value="/{name}")
+    void deleteTrainer(@PathVariable String name) {
+        this.trainerService.deleteTrainer(name);
+    }
+
+}
