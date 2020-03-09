ba61563cff2f551629934e4d3f26cb073f4086aa
elapauw <lapauwelise@gmail.com>
2020-03-09 10:41:15 +0100

    WIP security

--- src/test/java/com/miage/altea/trainer_api/controller/TrainerControllerIntegrationTest.java
+++ src/test/java/com/miage/altea/trainer_api/controller/TrainerControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package com.miage.altea.trainer_api.controller;
 import com.miage.altea.trainer_api.bo.Trainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -22,12 +23,18 @@ class TrainerControllerIntegrationTest {
     @Autowired
     private TrainerController controller;
 
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
     @Test
     void trainerController_shouldBeInstanciated(){
         assertNotNull(controller);
     }
 
-    @Test
+  /*  @Test
     void getTrainer_withNameAsh_shouldReturnAsh() {
         var ash = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/Ash", Trainer.class);
         assertNotNull(ash);
@@ -36,7 +43,7 @@ class TrainerControllerIntegrationTest {
 
         assertEquals(25, ash.getTeam().get(0).getPokemonTypeId());
         assertEquals(18, ash.getTeam().get(0).getLevel());
-    }
+    }*/
 
     @Test
     void getAllTrainers_shouldReturnAshAndMisty() {
@@ -46,5 +53,27 @@ class TrainerControllerIntegrationTest {
 
         assertEquals("Ash", trainers[0].getName());
         assertEquals("Misty", trainers[1].getName());
+    }
+
+    @Test
+    void getTrainers_shouldThrowAnUnauthorized(){
+        var responseEntity = this.restTemplate
+                .getForEntity("http://localhost:" + port + "/trainers/Ash", Trainer.class);
+        assertNotNull(responseEntity);
+        assertEquals(401, responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    void getTrainer_withNameAsh_shouldReturnAsh() {
+        var ash = this.restTemplate
+                .withBasicAuth(username, password)
+                .getForObject("http://localhost:" + port + "/trainers/Ash", Trainer.class);
+
+        assertNotNull(ash);
+        assertEquals("Ash", ash.getName());
+        assertEquals(1, ash.getTeam().size());
+
+        assertEquals(25, ash.getTeam().get(0).getPokemonTypeId());
+        assertEquals(18, ash.getTeam().get(0).getLevel());
     }
 }
